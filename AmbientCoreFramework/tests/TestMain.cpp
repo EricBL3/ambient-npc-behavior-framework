971d4df36b34cc93db161ab31fcbab6bc08465e0
EricBL3 <ericbuitron3@gmail.com>
2025-08-11 15:52:25 -0400

    Add vcpkg and gtest for project

---
+++ AmbientCoreFramework/tests/TestMain.cpp
@@ -0,0 +1,14 @@
+﻿#include <gtest/gtest.h>
+
+TEST(BasicTest, GoogleTestWorks)
+{
+    EXPECT_EQ(1 + 1, 2);
+    EXPECT_TRUE(true);
+}
+
+extern void hello();
+
+TEST(BasicTest, HelloFunctionExists)
+{
+    EXPECT_NO_THROW(hello());
+}
