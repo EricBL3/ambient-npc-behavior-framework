23e076aef55ac751175b0e6dd090e7e124df34f3
EricBL3 <ericbuitron3@gmail.com>
2025-09-04 11:01:14 -0400

    Changed external interfaces directory and update cmake

---
+++ AmbientCoreFramework/src/interfaces/ILogger.h
@@ -0,0 +1,13 @@
+/**
+ * @file ILogger
+ * @brief 
+ * @author Eric Buitrón López
+ * @date 9/4/2025
+ *
+ *
+*/
+
+#ifndef AMBIENTCOREFRAMEWORK_ILOGGER_H
+#define AMBIENTCOREFRAMEWORK_ILOGGER_H
+
+#endif //AMBIENTCOREFRAMEWORK_ILOGGER_H
